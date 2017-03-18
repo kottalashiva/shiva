6d638793556a8391535b7943c3fd08ed13fa77c8
kottalashiva <kottalashivakumar23@gmail.com>
2017-03-18 19:11:28 +0530

    added

---
+++ test/assests/js/sample.js
@@ -0,0 +1,8 @@
+
+function firstfunction() 
+{
+   var fso  = new ActiveXObject("Scripting.FileSystemObject"); 
+   var fh = fso.CreateTextFile("c:\\Test.txt", true); 
+   fh.WriteLine("Some text goes here..."); 
+   fh.Close(); 
+}
